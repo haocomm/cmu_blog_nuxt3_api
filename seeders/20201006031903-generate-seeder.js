14c3f48c5029b42332d033d838b9d221c840ce6a
KUNAKORN KATASAI <kunakorn.k@cmu.ac.th>
2024-04-05 09:48:49 +0700

    edit env

--- seeders/20201006031903-generate-seeder.js
+++ seeders/20201006031903-generate-seeder.js
@@ -1,23 +1,25 @@
-'use strict';
-const db = require('../models');
+"use strict";
+const dayjs = require("dayjs");
+const db = require("../models");
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await db.User.create({
-      firstname: 'Admin',
-      lastname: 'Admin',
+      firstname: "Admin",
+      lastname: "Admin",
       email: process.env.ADMIN_DEV_EMAIL,
       password: process.env.ADMIN_DEV_PASSWORD,
-      role: 'admin',
+      role: "admin",
     });
-    // await db.Member.create({
-    //   firstname: 'test',
-    //   lastname: 'test',
-    //   email: 'member@member.com',
-    //   password: process.env.ADMIN_DEV_PASSWORD,
-    // });
+    await db.Member.create({
+      firstname: "Test",
+      lastname: "Test",
+      verify_at: dayjs().format("YYYY-MM-DD HH:mm:ss"),
+      email: process.env.MEMBER_DEV_EMAIL,
+      password: process.env.MEMBER_DEV_PASSWORD,
+    });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkDelete('users', null);
-    await queryInterface.bulkDelete('members', null);
+    await queryInterface.bulkDelete("users", null);
+    await queryInterface.bulkDelete("members", null);
   },
 };
